248eda4f3ae2ac020c65ad53a17e2069d9f5e192
Greg Harris <gharris@angrylogic.net>
2014-03-09 20:09:13 -0400

    Update description

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@
 from distutils.core import setup
 
 setup(name="posixqueue", version="1.0", 
-      description="Python wrapper for POSIX message queues",
+      description="Python wrapper for POSIX message queue interface",
       author="Greg Harris", author_email="gharris@angrylogic.net",
       packages=["posixqueue"])
